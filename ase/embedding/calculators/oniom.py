7760a087bf168e7e0ab969ee86ca1aece22be46f
lopeza <lopeza@5af997e4-4c3d-0410-961e-ac0512e437d9>
2013-07-03 08:15:47 +0000

    small changes in oniom class, TODO the box selector

--- ase/embedding/calculators/oniom.py
+++ ase/embedding/calculators/oniom.py
@@ -1,14 +1,13 @@
 
 from ase.embedding.multiase.mixer.selector import AtomListSelector
-from ase.embedding.multiase.mixer.mixer import Mixer, EnergyCalculation, ForceCalculation
-
-import numpy as np
-
+from ase.embedding.multiase.mixer.mixer import Mixer, EnergyCalculation 
+from ase.embedding.multiase.mixer.mixer import ForceCalculation
 
 class ONIOM(Mixer):
     def __init__(self, atoms, calc_full=None, cell_full=None,
-                 calc_loc=None, cell_loc=None, atoms_loc=None):
-        """ The total energy and forces are a combination: 
+                 calc_loc=None, cell_loc=None, atoms_loc=None,
+                 selector='AtomListSelector'):
+        """ The total energy and forces follow the linear combination: 
         H = H_calc_full(cell_full) + ( H_calc_loc(cell_loc) 
                                        - H_calc_full(cell_loc))
           = H_1(A) + ( H_2(B) - H_1(B) )
@@ -16,18 +15,26 @@ class ONIOM(Mixer):
         atoms: ASE object with all atoms
         calc_full: ase-calculator for the full region
         cell_full: defines de size of the full region
-        calc_loc: ase-calculator for the localized region
-        cell_loc: defines the size of the localized region
-        atoms_loc: tuple with atom indices belonging to the localized region
+        calc_loc: ase-calculator for the local region
+        cell_loc: defines the size of the local region
+        atoms_loc: tuple with atom indices belonging to the local region
         """
 
         Mixer.set_atom_ids(atoms) # sequence of numbers in same order as 
                                #positions were given above, index starts from 0
 
-        #Create filter for the regions full (A) and localized (B)
-        filter_A = AtomListSelector((0, 1), {0: 1.0, 1: 1.0})        
-        filter_B = AtomListSelector((0, 1), {0:1.0, 1: 0.0})
-        
+        #Create filter for the regions full (A) and local (B)
+        if selector == 'AtomListSelector':
+            atoms_full = tuple(range(len(atoms)))
+            weights_full = {a:1.0 for a in atoms_full}
+            weights_loc = {a:0.0 for a in atoms_full}
+            for a in atoms_loc:
+                weights_loc[a] = 1.0
+            filter_A = AtomListSelector(atoms_full, weights_full)        
+            filter_B = AtomListSelector(atoms_full, weights_loc)
+        else: # TODO use the box selector implemented in mixer
+            raise ValueError("Invalid selector given")
+
         #Create energies and forces, default coefficient is 1.0
         #only H_1_B and F_1_B have negative coefficient
         forces_1_A = ForceCalculation("forces_full",
--- ase/embedding/tests/calculators/h2_oniom.py
+++ ase/embedding/tests/calculators/h2_oniom.py
@@ -2,6 +2,7 @@ from ase import Atoms
 from ase.embedding.calculators.oniom import ONIOM
 #import ase calculators for oniom type of embedding
 from gpaw import GPAW
+from ase.calculators.emt import EMT
 from ase.embedding.multiase.lammps.reaxff import ReaxFF
 from ase.embedding.multiase.utils import get_datafile
 
@@ -12,23 +13,28 @@ a = 6.0
 
 #create atoms
 atoms = Atoms("H2",
-                positions = [(0, 0, 0),
-                (0, 0, d)],
-                cell = (10*a, 10*a, 10*a))
+              positions = [(0, 0, 0),
+                           (0, 0, d)],
+              cell = (10*a, 10*a, 10*a))
 
 #Full region calculator and cell (also noted as MM region)
-calc_reaxff = ReaxFF(ff_file_path=get_datafile("ffield.reax.new"),
-                implementation="C")
-cell_reaxff = (100*a, 100*a, 100*a)
+calc_emt = EMT()
+cell_emt = (10*a,10*a,10*a)
 
-#Localized region calculator and cell (also noted as QM region)
+#Alternative full region calculator and cell (also noted as MM region)
+#to use them, replace replace the respective parameters in ONIOM()
+calc_reaxff = ReaxFF(ff_file_path=get_datafile("ffield.reax.new"),
+                     implementation="C")
+cell_reaxff = (10*a, 10*a, 10*a)
+
+#Local region calculator and cell (also noted as QM region)
 calc_gpaw = GPAW(nbands=2, txt="h2_1.txt")
 cell_gpaw = (a, a, a)
 #list of atoms belonging to the localized region
-atoms_gpaw = [1]
+atoms_gpaw = [0]
 
 #create oniom calculator
-oniom = ONIOM(atoms, calc_full=calc_reaxff, cell_full=cell_reaxff,
+oniom = ONIOM(atoms, calc_full=calc_emt, cell_full=cell_emt,
               calc_loc=calc_gpaw, cell_loc=cell_gpaw, atoms_loc=atoms_gpaw)
 
 #the created calculator can be used in the same way as any other ase calculator
