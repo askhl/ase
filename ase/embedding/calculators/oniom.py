7649a80724570143189ba3482bd3c7a05ccd0512
lopeza <lopeza@5af997e4-4c3d-0410-961e-ac0512e437d9>
2013-07-03 08:23:08 +0000

    use the notation A+B for full region

--- ase/embedding/calculators/oniom.py
+++ ase/embedding/calculators/oniom.py
@@ -10,7 +10,7 @@ class ONIOM(Mixer):
         """ The total energy and forces follow the linear combination: 
         H = H_calc_full(cell_full) + ( H_calc_loc(cell_loc) 
                                        - H_calc_full(cell_loc))
-          = H_1(A) + ( H_2(B) - H_1(B) )
+          = H_1(A+B) + ( H_2(B) - H_1(B) )
         Parameters in oniom
         atoms: ASE object with all atoms
         calc_full: ase-calculator for the full region
@@ -23,22 +23,22 @@ class ONIOM(Mixer):
         Mixer.set_atom_ids(atoms) # sequence of numbers in same order as 
                                #positions were given above, index starts from 0
 
-        #Create filter for the regions full (A) and local (B)
+        #Create filter for the regions full (A+B) and local (B)
         if selector == 'AtomListSelector':
             atoms_full = tuple(range(len(atoms)))
             weights_full = {a:1.0 for a in atoms_full}
             weights_loc = {a:0.0 for a in atoms_full}
             for a in atoms_loc:
                 weights_loc[a] = 1.0
-            filter_A = AtomListSelector(atoms_full, weights_full)        
+            filter_AB = AtomListSelector(atoms_full, weights_full)        
             filter_B = AtomListSelector(atoms_full, weights_loc)
         else: # TODO use the box selector implemented in mixer
             raise ValueError("Invalid selector given")
 
         #Create energies and forces, default coefficient is 1.0
         #only H_1_B and F_1_B have negative coefficient
-        forces_1_A = ForceCalculation("forces_full",
-                                       selector=filter_A,
+        forces_1_AB = ForceCalculation("forces_full",
+                                       selector=filter_AB,
                                        calculator=calc_full,
                                        cell=cell_full)
 
@@ -53,8 +53,8 @@ class ONIOM(Mixer):
                                       cell=cell_loc,
                                       coeff=-1.0)
         
-        energy_1_A = EnergyCalculation("energy_full",
-                                       selector=filter_A,
+        energy_1_AB = EnergyCalculation("energy_full",
+                                       selector=filter_AB,
                                        calculator=calc_full,
                                        cell=cell_full)
         energy_2_B = EnergyCalculation("energy_loc",
@@ -69,8 +69,8 @@ class ONIOM(Mixer):
                                        coeff=-1.0)
 
 
-        mixer_forces = (forces_1_A, forces_2_B, forces_1_B)
-        mixer_energies = (energy_1_A, energy_2_B, energy_1_B)
+        mixer_forces = (forces_1_AB, forces_2_B, forces_1_B)
+        mixer_energies = (energy_1_AB, energy_2_B, energy_1_B)
 
 
         atoms.center()
